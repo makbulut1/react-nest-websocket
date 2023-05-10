9b5ece2dd92cef407d0aa9ee29b5b79ca79c5240
makbulut42 <makbulut@student.42istanbul.com.tr>
2023-05-10 12:23:21 +0300

    [add]:backend nestjs, chat gateway

---
+++ nest/src/chat/chat.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { OnModuleInit } from '@nestjs/common';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+})
+export class ChatGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      msg: 'New Message',
+      content: body,
+    });
+  }
+}
+
